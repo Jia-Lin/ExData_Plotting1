14745d88a651e2576b3aeca3ada030139e318fde
Xu, Jia Lin <jlxu70@gmail.com>
2016-01-10 19:50:16 +0800

    remove another dupliate directory

--- Downloads/Coursera/Data Science/Exploratory Analysis/assignment1/plot1.R
+++
@@ -1,4 +0,0 @@
-hpcc <- subset(read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?"), as.Date(Date, "%d/%m/%Y") >= as.Date('02/01/2007', "%m/%d/%Y") & as.Date(Date, "%d/%m/%Y") <= as.Date('02/02/2007', "%m/%d/%Y"))
-png("plot1.png", width = 480, height = 480)
-hist(hpcc$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power", breaks=20)
-dev.off()
